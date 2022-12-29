43fd0fc10978c6f52e4205af6ca9d7657ec796c8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-12-29 20:46:15 +0800

    refactor: tweak the version of login api

--- lib/src/module/login.dart
+++ lib/src/module/login.dart
@@ -3,24 +3,20 @@ part of '../module.dart';
 //手机登录
 Handler login_cellphone = (query, cookie) {
   cookie.add(Cookie('os', 'pc'));
-  cookie.add(Cookie('appver', '2.9.7'));
+  cookie.add(Cookie('appver', '2.10.6'));
   final data = {
     'phone': query!['phone'],
     'countrycode': query['countrycode'],
-    'password': Encrypted(
-            md5.convert(utf8.encode(query['password'])).bytes as Uint8List)
-        .base16,
+    'password': Encrypted(md5.convert(utf8.encode(query['password'])).bytes as Uint8List).base16,
     'rememberLogin': 'true'
   };
 
-  return request('POST', 'https://music.163.com/api/login/cellphone', data,
-      crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
+  return request('POST', 'https://music.163.com/api/login/cellphone', data, crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
 };
 
 // 登录刷新
 Handler login_refresh = (query, cookie) {
-  return request('POST', 'https://music.163.com/weapi/login/token/refresh', {},
-      crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
+  return request('POST', 'https://music.163.com/weapi/login/token/refresh', {}, crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
 };
 
 // 登录状态 TODO: 以后再加
@@ -30,28 +26,21 @@ Handler login = (query, cookie) {
   cookie.add(Cookie('os', 'pc'));
   final data = {
     'username': query!['email'],
-    'password': Encrypted(
-            md5.convert(utf8.encode(query['password'])).bytes as Uint8List)
-        .base16,
+    'password': Encrypted(md5.convert(utf8.encode(query['password'])).bytes as Uint8List).base16,
     'rememberLogin': 'true'
   };
 
-  return request('POST', 'https://music.163.com/weapi/login', data,
-      crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
+  return request('POST', 'https://music.163.com/weapi/login', data, crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
 };
 
 // 退出登录
 Handler logout = (query, cookie) {
-  return request('POST', 'https://music.163.com/weapi/logout', {},
-      crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
+  return request('POST', 'https://music.163.com/weapi/logout', {}, crypto: Crypto.weapi, cookies: cookie, ua: 'pc');
 };
 
 // 检测手机号码是否已经注册
 Handler cellphone_existence_check = (query, cookie) {
-  final data = {
-    'cellphone': query!['phone'],
-    'countrycode': query['countrycode']
-  };
+  final data = {'cellphone': query!['phone'], 'countrycode': query['countrycode']};
   return eapiRequest(
     'http://music.163.com/eapi/cellphone/existence/check',
     '/api/cellphone/existence/check',
